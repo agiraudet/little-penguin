db84e82f3e7b932d6a56a6662272ae77a6501aff
agiraudet <alois.giraudet+github@gmail.com>
2023-09-18 12:47:46 +0200

    fixed ex03 code style

--- ex03/do_work.c
+++ ex03/do_work.c
@@ -12,9 +12,9 @@ int do_work(int *my_int, int retval)
 	int z;
 
 	for (x = 0; x < my_int; ++x)
-	udelay(10);
+		udelay(10);
 	if (y < 10)
-	pr_info("We slept a long time!");
+		pr_info("We slept a long time!");
 	z = x * y;
 	return z;
 }
